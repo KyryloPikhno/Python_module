563d3fd5157959306753c31d231ed8210b8ec876
KyryloPikhno <Mr.Good@i.ua>
2023-12-30 15:54:42 +0200

    lesson-1 update

---
+++ lesson-1/main.py
@@ -0,0 +1,40 @@
+# def my_fn(a, b):
+#     a = a + 1
+#     c = a + b
+#     return c
+
+# name = input("Enter ur name: ")
+
+# print(name)
+
+# print(name.capitalize())
+
+# print(dir(__builtins__))
+
+# PEP8 document
+# on save formatting
+
+# my_list = [1, 2, 3]
+
+# a = 10
+# print(my_list)
+
+
+# def my_fn():
+#     print("hi")
+
+
+# def hello(name):
+#     print(name)
+
+
+# hello("hello there!")
+
+
+def sum_nums(a, b):
+    sum = a + b
+    return sum
+
+
+res = sum_nums(20.4, 3)
+print("res", res)
