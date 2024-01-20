2f22a921463b5e0e2bfad93b4bfa859eff5893e7
KyryloPikhno <Mr.Good@i.ua>
2024-01-20 14:03:37 +0200

    update

---
+++ lesson-1/additional.py
@@ -0,0 +1,26 @@
+# f = 5.99999
+# i = int(f)
+# print(i)
+
+# p = 5
+# t = float(p)
+# print(t)
+
+# e = 5
+# r = str(e)
+# print(type(r))
+
+# only 0 it's False
+
+# i = 30 // 5
+# print(type(i))
+
+# t = 11 % 3
+# print(t)
+
+# print(5 != 6)
+
+# i = 2
+# i += 5
+# i %= 2
+# print(i)
