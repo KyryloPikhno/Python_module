01c86e35471493ea1b05f32b9ccc12ccb3745c82
KyryloPikhno <Mr.Good@i.ua>
2024-01-20 14:40:21 +0200

    update

--- lesson-1/additional.py
+++ lesson-1/additional.py
@@ -24,3 +24,42 @@
 # i += 5
 # i %= 2
 # print(i)
+
+# i = 7
+
+# if i == 6 or i == 5:
+#     print("it's 6 or 5")
+# elif i == 7 and True:
+#     print("it's 7")
+# elif i == 8:
+#     print("it's 8")
+# else:
+#     print("error")
+
+# i = -5
+# while i <= 5:
+#     print(i)
+#     i += 1
+# else:
+#     print("ok")
+
+# for i in range(10, 100, 5):
+#     print(i)
+
+# for i in range(50):
+#     if i == 5 or i == 6:
+#         # break
+#         continue
+#     print(i)
+# else:
+#     print("the end")
+# print("some code")
+
+# str = "hello, world!"
+
+# print(str[0])
+
+# for i in str:
+#     print(i)
+
+# print(str[0:5:2])
