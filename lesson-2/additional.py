30924b76c50afe189917a8d1a2d938eabd2ec771
KyryloPikhno <Mr.Good@i.ua>
2024-01-27 13:43:37 +0200

    update

--- lesson-2/additional.py
+++ lesson-2/additional.py
@@ -1,12 +1,67 @@
-l = [1, 2]
+# l = [1, 2]
 
-a, b = l
+# a, b = l
 
-tuple_1 = (1, 2)
+# tuple_1 = (1, 2)
 
-a = 5
-b = 7
+# a = 5
+# b = 7
 
-res = a, b
+# res = a, b
 
-b, a = (a, b)
+# b, a = (a, b)
+
+
+# def decor(func):
+#     def inner(*args, **kwargs):
+#         print("*" * 20)
+#         func(*args, **kwargs)
+#         print("*" * 20)
+
+#     return inner
+
+
+# @decor
+# @decor
+# @decor
+# def greeting(name):
+#     print(f"hello, {name}")
+
+
+# greeting("Lol")
+
+# inner = decor(greeting)
+
+# inner()
+
+# for i in range(5):
+#     pass
+
+
+# def counter():
+#     count = 0
+
+#     def inc():
+#         nonlocal count
+#         count += 1
+#         return count
+
+#     return inc
+
+
+# counter1 = counter()
+# counter2 = counter()
+
+# print(counter1())
+# print(counter1())
+# print(counter1())
+# print(counter2())
+# print(counter2())
+# print(counter2())
+# print(counter2())
+
+# l = [1, 2, 3, -1, 2, 3, 4]
+
+# l.sort()
+
+# print(l)
