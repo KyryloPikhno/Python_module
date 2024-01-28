d37506ccabe1809e8b32e8de05dd901b9676613e
KyryloPikhno <Mr.Good@i.ua>
2024-01-28 15:59:38 +0200

    update

--- lesson-3/additional.py
+++ lesson-3/additional.py
@@ -46,24 +46,64 @@
 # car.greeting()
 
 
-class User:
-    def __init__(self, name, age):
-        self.__name = name
-        self.age = age
+# class User:
+#     def __init__(self, name, age):
+#         self.__name = name
+#         self.age = age
 
-    def get_name(self):
-        return self.__name
+#     def get_name(self):
+#         return self.__name
 
-    def set_name(self, name):
-        self.__name = name
+#     def set_name(self, name):
+#         self.__name = name
 
-    def del_name(self):
-        del self.__name
+#     def del_name(self):
+#         del self.__name
 
 
-user = User("Kris", 18)
+# user = User("Kris", 18)
 # print(user.get_name())
 # user.set_name("Lol")
 # user.del_name()
 # print(user.get_name())
 # print(user.set_name("Lol"))
+
+
+# class User:
+#     def __init__(self, name, age):
+#         self.__name = name
+#         self.age = age
+
+#     def __get_name(self):
+#         return self.__name
+
+#     def __set_name(self, name):
+#         self.__name = name
+
+#     def __del_name(self):
+#         del self.__name
+
+#     name = property(fget=__get_name, fset=__set_name, fdel=__del_name)
+
+
+# user = User("Kris", 18)
+# user.name = "Test"
+# print(user.name)
+
+
+# class User:
+#     def __init__(self, name, age):
+#         self.__name = name
+#         self.age = age
+
+#     @property
+#     def name(self):
+#         return self.__name
+
+#     @name.fset
+#     def name(self, name):
+#         self.__name = name
+
+#     @name.fdel
+#     def name(self):
+#         del self.__name
