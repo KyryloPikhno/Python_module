ed5e81c3c355f4fe3699d048afeb2ed6342f3263
KyryloPikhno <Mr.Good@i.ua>
2024-01-30 16:21:27 +0200

    update

---
+++ lesson-4/additional.py
@@ -0,0 +1,57 @@
+# def gen(name):
+#     for ch in name:
+#         yield ch
+
+
+# g = gen("Kirill")
+# print(next(g))
+# print("something")
+# print(next(g))
+
+
+# def gen(name):
+#     for ch in name:
+#         yield ch
+#         print("lol")
+
+
+# g = gen("Kirill")
+
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+
+
+# l = [i for i in range(50000000)]
+# input()
+
+# g = (i for i in range(3))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+
+# for i in g:
+#     print(i)
+
+# try:
+#     fffffff
+#     print(6 / 0)
+#     int("fffffff")
+# except NameError as err:
+#     print(f"Error: {err}")
+# except ZeroDivisionError as err:
+#     print(f"Error: {err}")
+# except Exception as err:
+#     print(f"Error: {err}")
+# else:
+#     print("Success")
+# finally:
+#     print("Finally")
+
+
+# print("It works")
+
+# 25:00
