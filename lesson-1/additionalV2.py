4cc8630ce73f2f356b0c805a55c6ac54e79c653b
KyryloPikhno <Mr.Good@i.ua>
2024-04-27 22:22:13 +0300

    update

--- lesson-1/additionalV2.py
+++ lesson-1/additionalV2.py
@@ -7,3 +7,15 @@
 # max_num(10, 2, 22)
 
 # test PyCharm
+# python3 --version
+
+# str = "/gmail.com"
+# print(str.isspace())
+# print(str.isalpha())
+# print(str.endswith("com"))
+# print(str.startswith("g"))
+# print(str.lower())  # and the same with upper and capitalize
+# print(str.islower())
+# print(str.replace(".com", ".ua", 1))
+# print(str.split("."))
+# print(str.strip("/"))
